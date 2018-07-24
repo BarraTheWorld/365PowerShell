3ff87902130339dc7fd8e0bc6aa2d6eba499689f
BarraTheWorld <41654121+BarraTheWorld@users.noreply.github.com>
2018-07-25 09:37:17 +1000

    Added current files
    activityAlerts - functions for creating alerts once connected
    mainForm - GUI to call functions
    securityLogin - Function to log in to security and compliance center

---
+++ mainForm.ps1
@@ -0,0 +1,143 @@
+﻿# Script to connect to different Microsoft services
+#
+# Author: Jacob Shand & Benjamin Western
+# Date: 24/07/2018
+# 
+
+# Link files to this script (master
+# Same folder, called functions.ps1
+. "\activityAlerts.ps1"
+. "\securityLogin.ps1"
+# Prompt user for credential to use for services
+ 
+#$globalCred = "null"
+
+# Set global error pref so we can catch errors when logging in
+$Global:ErrorActionPreference='stop'
+
+# Check if modules are installed, if not install and import
+try { 
+    import-module msonline
+} catch {
+    install-module msonline
+    import-module msonline
+}
+
+try { 
+    import-module AzureAD
+} catch {
+    install-module AzureAD
+    import-module AzureAD
+}
+
+try { 
+    import-module AzureRM
+} catch {
+    install-module AzureRM
+    import-module AzureRM
+}
+
+<# This form was created using POSHGUI.com  a free online gui designer for PowerShell
+.NAME
+    ONGC Systems Cloud Management
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+#region begin GUI{ 
+
+$ONGCPowerShell                  = New-Object system.Windows.Forms.Form
+$ONGCPowerShell.ClientSize       = '900,300'
+$ONGCPowerShell.text             = "ONGC Systems Cloud Management"
+$ONGCPowerShell.BackColor        = "#ffffff"
+$ONGCPowerShell.TopMost          = $true
+
+$SharePoint                      = New-Object system.Windows.Forms.Button
+$SharePoint.BackColor            = "#4aa8e2"
+$SharePoint.text                 = "SharePoint"
+$SharePoint.width                = 250
+$SharePoint.height               = 50
+$SharePoint.location             = New-Object System.Drawing.Point(110,30)
+$SharePoint.Font                 = 'Microsoft Sans Serif,10,style=Bold'
+
+$Skype                           = New-Object system.Windows.Forms.Button
+$Skype.BackColor                 = "#4aa8e2"
+$Skype.text                      = "Skype"
+$Skype.width                     = 250
+$Skype.height                    = 50
+$Skype.location                  = New-Object System.Drawing.Point(500,30)
+$Skype.Font                      = 'Microsoft Sans Serif,10,style=Bold'
+
+$AdminPortal                     = New-Object system.Windows.Forms.Button
+$AdminPortal.BackColor           = "#4aa8e2"
+$AdminPortal.text                = "Admin Portal"
+$AdminPortal.width               = 250
+$AdminPortal.height              = 50
+$AdminPortal.location            = New-Object System.Drawing.Point(110,90)
+$AdminPortal.Font                = 'Microsoft Sans Serif,10,style=Bold'
+
+$SecurityCentre                  = New-Object system.Windows.Forms.Button
+$SecurityCentre.BackColor        = "#4aa8e2"
+$SecurityCentre.text             = "Security Centre"
+$SecurityCentre.width            = 250
+$SecurityCentre.height           = 50
+$SecurityCentre.location         = New-Object System.Drawing.Point(500,91)
+$SecurityCentre.Font             = 'Microsoft Sans Serif,10,style=Bold'
+
+$Teams                           = New-Object system.Windows.Forms.Button
+$Teams.BackColor                 = "#4aa8e2"
+$Teams.text                      = "Teams"
+$Teams.width                     = 250
+$Teams.height                    = 50
+$Teams.location                  = New-Object System.Drawing.Point(110,150)
+$Teams.Font                      = 'Microsoft Sans Serif,10,style=Bold'
+
+$Azure                           = New-Object system.Windows.Forms.Button
+$Azure.BackColor                 = "#4aa8e2"
+$Azure.text                      = "Azure"
+$Azure.width                     = 250
+$Azure.height                    = 50
+$Azure.location                  = New-Object System.Drawing.Point(500,151)
+$Azure.Font                      = 'Microsoft Sans Serif,10,style=Bold'
+
+$Exchange                        = New-Object system.Windows.Forms.Button
+$Exchange.BackColor              = "#4aa8e2"
+$Exchange.text                   = "Exchange"
+$Exchange.width                  = 250
+$Exchange.height                 = 50
+$Exchange.location               = New-Object System.Drawing.Point(110,210)
+$Exchange.Font                   = 'Microsoft Sans Serif,10,style=Bold'
+
+$ExitMain                        = New-Object system.Windows.Forms.Button
+$ExitMain.BackColor              = "#4aa8e2"
+$ExitMain.text                   = "Exit"
+$ExitMain.width                  = 250
+$ExitMain.height                 = 50
+$ExitMain.location               = New-Object System.Drawing.Point(500,209)
+$ExitMain.Font                   = 'Microsoft Sans Serif,10,style=Bold'
+$ExitMain.ForeColor              = "#000000"
+
+$ONGCPowerShell.controls.AddRange(@($SharePoint,$Skype,$AdminPortal,$SecurityCentre,$Teams,$Azure,$Exchange,$ExitMain))
+
+#region gui events {
+$SharePoint.Add_MouseClick({ securityLogin
+
+})
+$Skype.Add_MouseClick({  })
+$AdminPortal.Add_MouseClick({  })
+$SecurityCentre.Add_MouseClick({ activityAlerts jshand96@hotmail.com  })
+$Teams.Add_MouseClick({  })
+$Azure.Add_MouseClick({  })
+$Exchange.Add_MouseClick({  })
+$ExitMain.Add_MouseClick({ $ONGCPowerShell.Close() })
+$ONGCPowerShell.Add_MouseDoubleClick({  })
+$ONGCPowerShell.Add_Load({$globalCred = Get-Credential})
+#endregion events }
+
+#endregion GUI }
+
+
+#Write your logic code here
+
+[void]$ONGCPowerShell.ShowDialog()
---
+++ securityLogin.ps1
@@ -0,0 +1,10 @@
+﻿Function securityLogin {
+    #try {
+        connect-msolservice -credential $globalCred
+        $securitySession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $globalCred -Authentication Basic -AllowRedirection -AllowClobber
+        Import-PSSession $securitySession
+        write-host -foregroundcolor green "Successfully connected to O365 Security and Compliance Center"
+   #} catch {
+        write-host -ForegroundColor red "Failed to connect to eO365 Security and Compliance Center"
+   #}
+}
