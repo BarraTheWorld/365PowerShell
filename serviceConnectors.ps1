6aa76d0ee6bcefc7e3ae67e87d5f66e5ec727209
BarraTheWorld <41654121+BarraTheWorld@users.noreply.github.com>
2018-07-25 22:34:23 +1000

    Update serviceConnectors.ps1
    Adding "connecting to XXX" for more readbility

--- serviceConnectors.ps1
+++ serviceConnectors.ps1
@@ -1,4 +1,5 @@
 Function exchangeLogin {
+    write-host "Connecting to Exchange online...."
     try {
         $exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/?proxyMethod=RPS -Credential $globalCred -Authentication Basic
         Import-PSSession $exchangeSession
@@ -10,6 +11,7 @@ Function exchangeLogin {
 
 # Connect to O365 admin portal using given credential
 Function adminLogin {
+    write-host "Connecting to Admin Portal...."
     try{
         Connect-msolservice -credential $globalCred
         write-host -ForegroundColor green "Successfully connected to O365 Admin Portal!"
@@ -20,6 +22,7 @@ Function adminLogin {
 
 # Connect to Teams service using given credential
 Function teamsLogin {
+    write-host "Connecting to Teams Service...."
     try{
         Connect-MicrosoftTeams -credential $globalCred
         Write-Host -ForegroundColor Green "Successfully connected to Microsoft Teams service!"
@@ -30,6 +33,7 @@ Function teamsLogin {
 
 # Connect to Sharepoint Online Service using given credentials
 Function sharepointLogin{
+    write-host "Connecting to Sharepoint online...."
     try {
     } catch {
     }
@@ -37,18 +41,20 @@ Function sharepointLogin{
 }
 
 Function securityLogin {
-    #try {
+   write-host "Connecting to Security and Compliance Center...."
+   try {
         connect-msolservice -credential $globalCred
         $securitySession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $globalCred -Authentication Basic -AllowRedirection -AllowClobber
         Import-PSSession $securitySession
         write-host -foregroundcolor green "Successfully connected to O365 Security and Compliance Center"
-   #} catch {
+   } catch {
         write-host -ForegroundColor red "Failed to connect to eO365 Security and Compliance Center"
-   #}
+   }
 }
 # Connect to Skype for Business Online Portal using given credentials
 # CMDLet not working yet
 Function skypeLogin {
+    write-host "Connecting to Skype for Business online...."
     try {
         Connect-MsolService -Credential $globalCred
         $skypeSession = New-csonlinesession -credential $globalCred
@@ -59,6 +65,7 @@ Function skypeLogin {
 }
 
 Function azureLogin {
+    write-host "Connecting to Azure AD service...."
     try {
         Connect-AzureAD -Credential $globalCred
         Write-Host -ForegroundColor green "Successfully connected to the Azure AD service"
