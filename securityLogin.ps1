ee7d72edd48ddd4659cd6688a9379b1946291d5b
BarraTheWorld <41654121+BarraTheWorld@users.noreply.github.com>
2018-07-25 11:53:26 +1000

    Added other connectors and renamed
    Add all connectors for different services. Almost all are working

--- securityLogin.ps1
+++ securityLogin.ps1
@@ -1,4 +1,38 @@
-﻿Function securityLogin {
+Function exchangeOnline {
+    $exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/?proxyMethod=RPS -Credential $globalCred -Authentication Basic
+    Import-PSSession $exchangeSession
+    write-host -foregroundcolor green "Successfully connected to Exchange online!"
+}
+
+# Connect to O365 admin portal using given credential
+Function adminLogin {
+    try{
+        Connect-msolservice -credential $globalCred
+        write-host -ForegroundColor green "Successfully connected to O365 Admin Portal!"
+    } catch {
+        write-host -ForegroundColor red "Failed to connect to O365 admin portal!"
+    }
+}
+
+# Connect to Teams service using given credential
+Function teamsLogin {
+    try{
+        Connect-MicrosoftTeams -credential $globalCred
+        Write-Host -ForegroundColor Green "Successfully connected to Microsoft Teams service!"
+    } catch {
+        Write-Host -ForegroundColor Red "Failed to connect to Microsoft Teams service!"
+    }
+}
+
+# Connect to Sharepoint Online Service using given credentials
+Function sharepointLogin{
+    try {
+    } catch {
+    }
+    ## Need to prompt user for SPO site details
+}
+
+Function securityLogin {
     #try {
         connect-msolservice -credential $globalCred
         $securitySession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $globalCred -Authentication Basic -AllowRedirection -AllowClobber
@@ -7,4 +41,24 @@
    #} catch {
         write-host -ForegroundColor red "Failed to connect to eO365 Security and Compliance Center"
    #}
+}
+# Connect to Skype for Business Online Portal using given credentials
+# CMDLet not working yet
+Function skypeOnline {
+    try {
+        Connect-MsolService -Credential $globalCred
+        $skypeSession = New-csonlinesession -credential $globalCred
+        write-host -ForegroundColor green "Successfully connected to Skype for Business Online Services"
+    } catch {
+        write-host -ForegroundColor red "Failed to connect Skype for Business Online Services"
+    }  
+}
+
+Function azureAD {
+    try {
+        Connect-AzureAD -Credential $globalCred
+        Write-Host -ForegroundColor green "Successfully connected to the Azure AD service"
+    } catch {
+        Write-Host -ForegroundColor red "Failed to conenct to the Azure AD service"
+    }
 }
