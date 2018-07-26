404403cb2021e22d5fe1a8d477d03ecc4a5e3e15
SrBushWookie <41654926+SrBushWookie@users.noreply.github.com>
2018-07-27 07:21:00 +1000

    Update resetPassword.ps1

--- resetPassword.ps1
+++ resetPassword.ps1
@@ -1,5 +1,5 @@
 ﻿# Please note this will NOT work for cloud only users, they MUST 
-# use the MSOnline service (don't as don't tell)
+# use the MSOnline service (don't ask don't tell)
 
 Function passwordReset {
     Param ($userAccount, $newPassword )
