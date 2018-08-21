f89cad267a7aca44cc8aa5f3b2ba39ce6c90678d
BarraTheWorld <41654121+BarraTheWorld@users.noreply.github.com>
2018-08-21 11:30:50 +1000

    Fix to run all at the same time

--- serviceConnectors.ps1
+++ serviceConnectors.ps1
@@ -1,8 +1,42 @@
+Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
+
+try { 
+    import-module msonline
+} catch {
+    install-module msonline -Force
+    import-module msonline -Force
+}
+
+try {
+    import-module MicrosoftTeams
+} catch {
+    install-module MicrosoftTeams -Force
+    import-module MicrosoftTeams -Force
+}
+
+try { 
+    import-module AzureAD
+} catch {
+    install-module AzureAD -Force
+    import-module AzureAD -Force
+}
+
+try { 
+    import-module AzureRM
+} catch {
+    install-module AzureRM -Force
+    import-module AzureRM -Force
+}
+
+$globalCred = Get-Credential
+$domain = Read-Host "Please enter your domain: "
+
+
 Function exchangeLogin {
     write-host "Connecting to Exchange online...."
     try {
         $exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/?proxyMethod=RPS -Credential $globalCred -Authentication Basic
-        Import-PSSession $exchangeSession
+        Import-PSSession $exchangeSession | Out-Null
         write-host -foregroundcolor green "Successfully connected to Exchange online!"
     } catch {
         write-host -foregroundcolor red "Failed to connect to Exchange online!"
@@ -13,7 +47,7 @@ Function exchangeLogin {
 Function adminLogin {
     write-host "Connecting to Admin Portal...."
     try{
-        Connect-msolservice -credential $globalCred
+        Connect-msolservice -credential $globalCred | Out-Null
         write-host -ForegroundColor green "Successfully connected to O365 Admin Portal!"
     } catch {
         write-host -ForegroundColor red "Failed to connect to O365 admin portal!"
@@ -24,8 +58,8 @@ Function adminLogin {
 Function teamsLogin {
     write-host "Connecting to Teams Service...."
     try{
-        Connect-MicrosoftTeams -credential $globalCred
-        Write-Host -ForegroundColor Green "Successfully connected to Microsoft Teams service!"
+        Connect-MicrosoftTeams -credential $globalCred | Out-Null
+        Write-Host -ForegroundColor Green "Successfully connected to Teams service!"
     } catch {
         Write-Host -ForegroundColor Red "Failed to connect to Microsoft Teams service!"
     }
@@ -35,20 +69,22 @@ Function teamsLogin {
 Function sharepointLogin{
     write-host "Connecting to Sharepoint online...."
     try {
+        Connect-SPOService -Url https://$domain-admin.sharepoint.com -credential $globalCred | Out-Null
+        Write-Host -ForegroundColor Green "Successfully connected to Sharepoint admin!"
     } catch {
+        Write-Host -ForegroundColor red "Failed to connect to Sharepoint Admin"
     }
-    ## Need to prompt user for SPO site details
 }
 
 Function securityLogin {
    write-host "Connecting to Security and Compliance Center...."
    try {
-        connect-msolservice -credential $globalCred
-        $securitySession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $globalCred -Authentication Basic -AllowRedirection -AllowClobber
-        Import-PSSession $securitySession
+        connect-msolservice -credential $globalCred | Out-Null
+        $securitySession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $globalCred -Authentication Basic -AllowRedirection
+        Import-PSSession $securitySession | Out-Null
         write-host -foregroundcolor green "Successfully connected to O365 Security and Compliance Center"
    } catch {
-        write-host -ForegroundColor red "Failed to connect to eO365 Security and Compliance Center"
+       write-host -ForegroundColor red "Failed to connect to eO365 Security and Compliance Center"
    }
 }
 # Connect to Skype for Business Online Portal using given credentials
@@ -58,6 +94,7 @@ Function skypeLogin {
     try {
         Connect-MsolService -Credential $globalCred
         $skypeSession = New-csonlinesession -credential $globalCred
+        Import-PSSession $skypeSession | Out-Null
         write-host -ForegroundColor green "Successfully connected to Skype for Business Online Services"
     } catch {
         write-host -ForegroundColor red "Failed to connect Skype for Business Online Services"
@@ -67,9 +104,19 @@ Function skypeLogin {
 Function azureLogin {
     write-host "Connecting to Azure AD service...."
     try {
-        Connect-AzureAD -Credential $globalCred
+        Connect-AzureAD -Credential $globalCred | Out-Null
         Write-Host -ForegroundColor green "Successfully connected to the Azure AD service"
     } catch {
         Write-Host -ForegroundColor red "Failed to conenct to the Azure AD service"
     }
+}
+
+Function connectAll {
+    exchangeLogin
+    adminLogin
+    teamsLogin
+    sharepointLogin
+    securityLogin
+    skypeLogin
+    azureLogin
 }
