c292cfc96248ccfa592a3d48900d12b7f7f8f08e
Jessica Phillips <benjaminswestern@outlook.com>
2018-11-05 16:54:11 +1000

    further new scripts

---
+++ vhdToFolderMigration.ps1
@@ -0,0 +1,38 @@
+﻿#New-VHD -Path "C:\test2.vhdx" -Fixed -SizeBytes 3GB
+
+$copyLog = C:\errorvhdcopy.txt
+
+$vhdName = Get-ChildItem | Where-Object -Property Name -Match ".vhdx" | Get-VHD | Select-Object Path
+
+
+
+function createFolders {
+
+Mount-VHD -Path $vhdName.Path
+
+foreach ($vhdNames in $vhdName) {
+
+$changedVhdName = $vhdNames
+
+$changedVhdName = $changedVhdName -replace '[@{}Path=c:\\.vhdx]'
+
+New-Item -ItemType Directory -Path $changedVhdName
+
+}
+                        }
+
+
+
+function copyContents {
+
+robocopy $vhdName $changedVhdName *.* /mov /e /w:1 /r:1 /tee /np /XO /xd "`$RECYCLE.BIN" /xf "desktop.ini" /log:$copyLog /v
+
+                       }
+
+
+
+function undoVhd{
+
+Dismount-VHD -Path $VhdName.Path
+
+}
